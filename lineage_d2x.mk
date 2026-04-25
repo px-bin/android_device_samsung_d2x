8ecfe32380d593ca04ee223800b2c40991a08708
pedro <pxd.bin@gmail.com>
2026-04-25 09:00:21 -0300

    enbale UDFPS customization

--- lineage_d2x.mk
+++ lineage_d2x.mk
@@ -42,6 +42,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_SUPPORTS_BLUR := true
+TARGET_HAS_UDFPS := true
 
 ## Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
